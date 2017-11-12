f4c3626504ac640e4fda7d7a91b01b107f35f78b
Chely Ho <adept_inept@yahoo.com>
2017-11-12 02:06:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,24 @@ class Ceo extends BoardMember{
     return "Welcome aboard!";
   }
 }
+
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+ 
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+ 
+ 
+class Teacher extends User {
+  sayHello(){
+    super.sayHello()
+    console.log('hello')
+  }
+}
+ 
+tom.sayHello()
