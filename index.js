522f466276a2f5c3fc1927cba560545e881181fd
Chely Ho <adept_inept@yahoo.com>
2017-11-12 02:07:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,18 +35,20 @@ class User {
     this.name = name;
     this.email = email;
   }
- 
+
   sayHello() {
     console.log(`Hello, my name is ${this.name}`);
   }
 }
- 
- 
+
+
 class Teacher extends User {
   sayHello(){
     super.sayHello()
     console.log('hello')
   }
 }
- 
+
 tom.sayHello()
+
+console.log('yo')
